0a96ec32a560aaef1c1738d61f08c2a526977445
Mark Levitt <mark@marklevitt.co.uk>
2019-05-20 15:01:32 +0100

    Add a greater than operator to MusicClass

--- tests/test_MusicFileClass.py
+++ tests/test_MusicFileClass.py
@@ -34,3 +34,43 @@ def test_musicfile_returns_full_path_name(music_file):
 
 def test_musicfile_returns_parent_path(music_file):
     assert music_file["class"].album == music_file["track"].parent.resolve()
+
+
+def test_files_are_equal(test_tracks):
+    assert test_tracks["equal"].longer == test_tracks["equal"].shorter
+
+
+def test_files_are_not_equal(test_tracks):
+    assert test_tracks["equal"].longer != test_tracks["better_worse"].worse
+
+
+def test_should_return_false_if_not_equal_is_false(test_tracks):
+    assert (test_tracks["equal"].longer != test_tracks["equal"].shorter) is False
+
+
+def test_higher_bitrate_file_greater_than_lower_bitrate_file(test_tracks):
+    assert test_tracks["better_worse"].better > test_tracks["better_worse"].worse
+
+
+def test_lower_bitrate_file_less_than_higher_bitrate_file(test_tracks):
+    assert test_tracks["better_worse"].worse < test_tracks["better_worse"].better
+
+
+def test_shorter_name_file_greater_than_longer_name_file(test_tracks):
+    assert test_tracks["equal"].shorter > test_tracks["equal"].longer
+
+
+def test_longer_name_file_less_than_shorter_name_file(test_tracks):
+    assert test_tracks["equal"].longer < test_tracks["equal"].shorter
+
+
+def test_higher_bitrate_file_greater_than_shorter_name_file(test_tracks):
+    assert test_tracks["better_worse"].better > test_tracks["short128bit"]
+
+
+def test_shorter_name_file_less_than_higher_bitrate_file(test_tracks):
+    assert test_tracks["short128bit"] < test_tracks["better_worse"].better
+
+
+def test_should_return_false_if_greater_than_is_false(test_tracks):
+    assert (test_tracks["short128bit"] > test_tracks["better_worse"].better) is False
