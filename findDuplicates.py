f202261b034b902317fe8906551183f0ad09799f
Mark Levitt <mark@marklevitt.co.uk>
2019-05-20 11:34:52 +0100

    Move MusicFile class into its own module, split up tests

--- findDuplicates.py
+++ findDuplicates.py
@@ -1,47 +1,10 @@
-from tinytag import TinyTag
+from musicfile import MusicFile
 from pathlib import Path
 import sys
 
 actually_delete = False
 
 
-class MusicFile(object):
-
-    def __init__(self, filename):
-        self.path = Path(filename)
-
-    @property
-    def bitrate(self):
-        return TinyTag.get(self.path).bitrate
-
-    @property
-    def size(self):
-        return Path.stat(self.path).st_size
-
-    @property
-    def full_path_name(self):
-        return self.__str__()
-
-    @property
-    def name(self):
-        return self.__repr__()
-
-    @property
-    def album(self):
-        return self.path.parent.resolve()
-
-    def __repr__(self):
-        return self.path.name
-
-    def __str__(self):
-        return str(self.path.resolve())
-
-    def __eq__(self, other):
-        """Files are equal if they have the same size and bitrate"""
-        if isinstance(other, MusicFile):
-            return (self.bitrate == other.bitrate) and (self.size == other.size)
-        else:
-            return NotImplemented
 
 
 def all_files(starting_path=".", pattern="*"):
--- tests/test_findDuplicates.py
+++ tests/test_findDuplicates.py
@@ -3,57 +3,6 @@ from collections import namedtuple
 
 from findDuplicates import *
 
-@pytest.fixture
-def music_file():
-    best_file = Path("resources/amazing_track.m4a")
-    return {"class": MusicFile(filename=best_file),
-            "track": best_file}
-
-
-@pytest.fixture
-def test_tracks():
-    best_file = Path("resources/amazing_track.m4a")
-    worst_file = Path("resources/amazing_track 1.m4a")
-
-    first_equal = Path("resources/Equal 1.m4a")
-    second_equal = Path("resources/Equal.m4a")
-
-    BetterWorse = namedtuple('BetterWorse', ['better', 'worse'])
-    Equal = namedtuple('Equal', ['longer', 'shorter'])
-
-    return {"better_worse": BetterWorse(MusicFile(best_file), MusicFile(worst_file)),
-            "equal": Equal(MusicFile(first_equal), MusicFile(second_equal))}
-
-
-def test_musicfile_class(music_file):
-    assert isinstance(music_file["class"], MusicFile)
-
-
-def test_musicfile_returns_bitrate(music_file):
-    assert music_file["class"].bitrate == 256
-    assert type(music_file["class"].bitrate) == float
-
-
-def test_musicfile_returns_size(music_file):
-    assert music_file["class"].size == 798880
-    assert type(music_file["class"].size) == int
-
-
-def test_musicfile_returns_path(music_file):
-    assert music_file["class"].path == music_file["track"]
-    assert isinstance(music_file["class"].path, Path)
-
-
-def test_musicfile_returns_short_name(music_file):
-    assert music_file["class"].name == music_file["track"].name
-
-
-def test_musicfile_returns_full_path_name(music_file):
-    assert music_file["class"].full_path_name == str(music_file["track"].resolve())
-
-
-def test_musicfile_returns_parent_path(music_file):
-    assert music_file["class"].album == music_file["track"].parent.resolve()
 
 
 def test_all_files():
