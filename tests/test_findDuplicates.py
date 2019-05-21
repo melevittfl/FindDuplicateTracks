80466da2be90424f3dc67f06476652ed7f31b0ab
Mark Levitt <mark@marklevitt.co.uk>
2019-05-21 21:12:13 +0100

    Refactor the comparison to return a tuple of tracks to keep and delete which means we don't have to subtrack one set from another. Significant speedup

--- tests/test_findDuplicates.py
+++ tests/test_findDuplicates.py
@@ -20,24 +20,25 @@ def test_files_are_not_equal(test_tracks):
 def test_best_track(test_tracks):
     # Different bitrate files should return the higher one
     assert best_track(test_tracks["better_worse"].better,
-                      test_tracks["better_worse"].worse) == test_tracks["better_worse"].better
+                      test_tracks["better_worse"].worse) == (test_tracks["better_worse"].better,
+                                                             test_tracks["better_worse"].worse)
 
     # Equal size and bitrate files should return the shorter name
     assert best_track(test_tracks["equal"].longer,
-                      test_tracks["equal"].shorter) == test_tracks["equal"].shorter
+                      test_tracks["equal"].shorter) == (test_tracks["equal"].shorter, test_tracks["equal"].longer)
 
     # Comparing the first file with None should return the first file
-    assert best_track(test_tracks["equal"].shorter, None) == test_tracks["equal"].shorter
+    assert best_track(test_tracks["equal"].shorter, None) == (test_tracks["equal"].shorter, None)
 
     # Comparing the None with the second file should return the second file
-    assert best_track(None, test_tracks["equal"].shorter) == test_tracks["equal"].shorter
+    assert best_track(None, test_tracks["equal"].shorter) == (test_tracks["equal"].shorter, None)
 
     # Comparing a MusicFile with a non MusicFile should return TypeError
     with pytest.raises(TypeError):
         best_track(test_tracks["equal"].longer, "Not A File")
 
 
-def test_evaluate_tracks_at_path(test_tracks):
+def test_find_tracks_to_delete_at_path(test_tracks):
     complete = [test_tracks["better_worse"].better,
                 test_tracks["better_worse"].worse,
                 test_tracks["equal"].shorter,
@@ -47,28 +48,11 @@ def test_evaluate_tracks_at_path(test_tracks):
     tracks_to_keep = [test_tracks["better_worse"].better,
                       test_tracks["equal"].shorter,
                       test_tracks["short128bit"]]
-    result = evaluate_tracks_at_path("resources")
-    assert isinstance(result["keep"][0], MusicFile)
-    assert isinstance(result["all"][0], MusicFile)
-    assert set(result["all"]) == set(complete)
-    assert set(result["keep"]) == set(tracks_to_keep)
 
-
-def test_generate_delete_list(test_tracks):
-    complete = [test_tracks["better_worse"].better,
-                test_tracks["better_worse"].worse,
-                test_tracks["equal"].shorter,
-                test_tracks["equal"].longer,
-                test_tracks["short128bit"]]
-
-    tracks_to_keep = [test_tracks["better_worse"].better,
-                     test_tracks["equal"].shorter,
-                     test_tracks["short128bit"]]
-
-    result = generate_delete_list(complete, tracks_to_keep)
-    should_be_empty = result.intersection(tracks_to_keep)
-
-    assert len(should_be_empty) == 0
+    list_to_delete = set(complete) - set(tracks_to_keep)
+    result = find_tracks_to_delete_at_path("resources")
+    assert isinstance(result[0], MusicFile)
+    assert set(result) == set(list_to_delete)
 
 
 def test_delete_tracks(tmpdir):
